4400e4871ea1588e9cec4c7c4abb036780144bd2
izikenmichael <izikenmichael@gmail.com>
2018-11-13 20:53:10 +0100

    Completed project with all requirements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwMoviesService } from './sw-movies/sw-movies.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'starWarsLocal';
+
+  constructor(
+    private moviesService: SwMoviesService
+  ) {}
+
+  ngOnInit(): void {
+    this.moviesService.getMoviesData();
+  }
 }
